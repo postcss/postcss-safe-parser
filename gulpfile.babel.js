1d73c82cec4fd4288d98f0e47dfc258ec2ba1cb0
Andrey Sitnik <andrey@sitnik.ru>
2015-08-07 14:42:16 +0300

    Remove prepublish hook

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,7 +17,7 @@ gulp.task('build:lib', ['clean'], () => {
 gulp.task('build:docs', ['clean'], () => {
     let ignore = require('fs').readFileSync('.npmignore').toString()
         .trim().split(/\n+/)
-        .concat(['.npmignore', 'index.js', 'package.json'])
+        .concat(['.npmignore', 'package.json'])
         .map( i => '!' + i );
     return gulp.src(['*'].concat(ignore))
         .pipe(gulp.dest('build'));
@@ -51,14 +51,16 @@ gulp.task('lint', () => {
 // Test
 
 gulp.task('test', () => {
-    require('./');
+    require('babel-core/register')({ extensions: ['.es6'], ignore: false });
     let mocha = require('gulp-mocha');
     return gulp.src('test/*.es6', { read: false }).pipe(mocha());
 });
 
 gulp.task('integration', (done) => {
+    require('babel-core/register')({ extensions: ['.es6'], ignore: false });
     let real = require('postcss-parser-tests/real');
     let safe = require('./');
+    
     real(done, [['Browserhacks', 'http://browserhacks.com/']], (css) => {
         return safe(css).toResult({ map: { annotation: false } });
     });
