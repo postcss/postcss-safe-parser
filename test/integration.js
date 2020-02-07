6414ec484154930a46a74dfb80266c54d7107b64
Andrey Sitnik <andrey@sitnik.ru>
2020-02-07 12:08:43 -0800

    Run integration test only on first CI

--- test/integration.js
+++ test/integration.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
+let ciJobNumber = require('ci-job-number')
 let real = require('postcss-parser-tests/real')
 
 let safe = require('../build')
 
-real(() => true, [['Browserhacks', 'http://browserhacks.com/']], css => {
-  return safe(css).toResult({ map: { annotation: false } })
-})
+if (ciJobNumber() === 1) {
+  real(() => true, [['Browserhacks', 'http://browserhacks.com/']], css => {
+    return safe(css).toResult({ map: { annotation: false } })
+  })
+}
