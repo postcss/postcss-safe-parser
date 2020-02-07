0dec3f1f30cc0c5f0c63400e20e808f18a74add4
Andrey Sitnik <andrey@sitnik.ru>
2020-02-07 12:36:32 -0800

    Use direct link to hacks

--- test/integration.js
+++ test/integration.js
@@ -5,8 +5,12 @@ let real = require('postcss-parser-tests/real')
 
 let safe = require('../build')
 
+const BROWSERHACKS =
+  'https://raw.githubusercontent.com/maste/browserhacks/gh-pages/' +
+  'assets/css/tests.css'
+
 if (ciJobNumber() === 1) {
-  real(() => true, [['Browserhacks', 'http://browserhacks.com/']], css => {
+  real(() => true, [['Browserhacks', BROWSERHACKS]], css => {
     return safe(css).toResult({ map: { annotation: false } })
   })
 }
