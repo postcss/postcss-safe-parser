0c31026ac7809d9901ead5cb425913eafffc54ea
Andrey Sitnik <andrey@sitnik.ru>
2021-06-14 02:16:10 -0400

    Update development practices

--- test/integration.js
+++ test/integration.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 let { testOnReal } = require('postcss-parser-tests')
-let ciJobNumber = require('ci-job-number')
 
 let safe = require('../lib/safe-parse')
 
-if (ciJobNumber() === 1) {
-  testOnReal(css => safe(css).toResult({ map: { annotation: false } }), [
+testOnReal(
+  css => safe(css).toResult({ map: { annotation: false } }),
+  [
     'https://raw.githubusercontent.com/' +
       '4ae9b8/browserhacks/gh-pages/assets/css/tests.css'
-  ])
-}
+  ]
+)
