fe517597de0056d57897b541fe771b2c84b94bd4
Andrey Sitnik <andrey@sitnik.ru>
2021-10-20 15:48:55 +0200

    Update dependnecies

--- test/parse.test.js
+++ test/parse.test.js
@@ -12,61 +12,61 @@ eachTest((name, css, json) => {
 })
 
 it('fixes unclosed blocks in safe mode', () => {
-  expect(parse('@media (screen) { a {\n').toString()).toEqual(
+  expect(parse('@media (screen) { a {\n').toString()).toBe(
     '@media (screen) { a {\n}}'
   )
-  expect(parse('a { color').toString()).toEqual('a { color}')
-  expect(parse('a { color: black').first.first.prop).toEqual('color')
+  expect(parse('a { color').toString()).toBe('a { color}')
+  expect(parse('a { color: black').first.first.prop).toBe('color')
 })
 
 it('fixes unnecessary block close in safe mode', () => {
   let root = parse('a {\n} }')
-  expect(root.first.toString()).toEqual('a {\n}')
-  expect(root.raws.after).toEqual(' }')
+  expect(root.first.toString()).toBe('a {\n}')
+  expect(root.raws.after).toBe(' }')
 })
 
 it('fixes unclosed comment in safe mode', () => {
   let root = parse('a { /* b ')
-  expect(root.toString()).toEqual('a { /* b */}')
-  expect(root.first.first.text).toEqual('b')
+  expect(root.toString()).toBe('a { /* b */}')
+  expect(root.first.first.text).toBe('b')
 })
 
 it('fixes column and semicolumn case', () => {
-  expect(parse('a{:;}').toString()).toEqual('a{}')
+  expect(parse('a{:;}').toString()).toBe('a{}')
 })
 
 it('fixes unclosed quote in safe mode', () => {
-  expect(parse('a { content: "b').first.first.value).toEqual('"b')
+  expect(parse('a { content: "b').first.first.value).toBe('"b')
 })
 
 it('fixes unclosed bracket', () => {
-  expect(parse(':not(one() { }').toString()).toEqual(':not(one() { }')
+  expect(parse(':not(one() { }').toString()).toBe(':not(one() { }')
 })
 
 it('fixes property without value in safe mode', () => {
   let root = parse('a { color: white; one }')
   expect(root.first.nodes).toHaveLength(1)
   expect(root.first.raws.semicolon).toBe(true)
-  expect(root.first.raws.after).toEqual(' one ')
+  expect(root.first.raws.after).toBe(' one ')
 })
 
 it('fixes 2 properties in safe mode', () => {
   let root = parse('a { color one: white; one }')
   expect(root.first.nodes).toHaveLength(1)
-  expect(root.first.first.prop).toEqual('color')
-  expect(root.first.first.raws.between).toEqual(' one: ')
+  expect(root.first.first.prop).toBe('color')
+  expect(root.first.first.raws.between).toBe(' one: ')
 })
 
 it('fixes nameless at-rule in safe mode', () => {
   let root = parse('@')
-  expect(root.first.type).toEqual('atrule')
-  expect(root.first.name).toEqual('')
+  expect(root.first.type).toBe('atrule')
+  expect(root.first.name).toBe('')
 })
 
 it('fixes property without semicolon in safe mode', () => {
   let root = parse('a { one: 1 two: 2 }')
   expect(root.first.nodes).toHaveLength(2)
-  expect(root.toString()).toEqual('a { one: 1; two: 2 }')
+  expect(root.toString()).toBe('a { one: 1; two: 2 }')
 })
 
 it('does not fall on missed semicolon in IE filter', () => {
@@ -77,10 +77,10 @@ it('does not fall on missed semicolon in IE filter', () => {
 
 it('fixes double colon in safe mode', () => {
   let root = parse('a { one:: 1 }')
-  expect(root.first.first.value).toEqual(': 1')
+  expect(root.first.first.value).toBe(': 1')
 })
 
 it('fixes colon instead of semicolon', () => {
   let root = parse('a { one: 1: } b { one: 1 : }')
-  expect(root.toString()).toEqual('a { one: 1: } b { one: 1 : }')
+  expect(root.toString()).toBe('a { one: 1: } b { one: 1 : }')
 })
